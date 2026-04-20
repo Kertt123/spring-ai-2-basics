6f951e3bec2bf9cc592ef367e04a602b06aff480
Krzysztof_Serkowski <krzysztof_serkowski@epam.com>
2026-04-20 12:58:00 +0200

    Update

--- src/main/java/com/serkowski/services/ChatService.java
+++ src/main/java/com/serkowski/services/ChatService.java
@@ -3,11 +3,7 @@ package com.serkowski.services;
 import com.serkowski.model.text.MovieRecommendationResponse;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.MimeType;
 import reactor.core.publisher.Flux;
 
 @Service
@@ -32,46 +28,6 @@ public class ChatService {
                 .user(message)
                 .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, conversationId))
                 .stream()
-                .content();
-    }
-
-    public String getCompletionsWithImageUrl(String message, String imgType, String imageUrl, String conversationId) {
-        try {
-            UrlResource resource = new UrlResource(imageUrl);
-            return chatClient.prompt()
-                    .user(userSpec -> userSpec
-                            .text(message)
-                            .media(MimeType.valueOf(imgType), resource))
-                    .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, conversationId))
-                    .call()
-                    .content();
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Not correct image URL " + imageUrl, e);
-        }
-    }
-
-    public String getCompletionsWithImagePath(String message, String imgType, String imgPath, String conversationId) {
-        try {
-            ClassPathResource resource = new ClassPathResource(imgPath);
-            return chatClient.prompt()
-                    .user(userSpec -> userSpec
-                            .text(message)
-                            .media(MimeType.valueOf(imgType), resource))
-                    .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, conversationId))
-                    .call()
-                    .content();
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Not correct image path " + imgPath, e);
-        }
-    }
-
-    public String getCompletionsWithImage(String message, String imgType, byte[] image, String conversationId) {
-        return chatClient.prompt()
-                .user(userSpec -> userSpec
-                        .text(message)
-                        .media(MimeType.valueOf(imgType), new ByteArrayResource(image)))
-                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, conversationId))
-                .call()
                 .content();
     }
 
